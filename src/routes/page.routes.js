e9bc1e26387d45c34792f0a494df01fcc880835f
hensonsagorsor-collab <henson.sagorsor@e.ubaguio.edu>
2026-02-03 17:41:42 +0800

    feat: Refine login UI/UX - compact & mobile-friendly design

--- src/routes/page.routes.js
+++ src/routes/page.routes.js
@@ -1,8 +1,52 @@
 const express = require('express');
 const router = express.Router();
-const pageController = require('../controllers/page.controller');
 
-router.get('/', pageController.getHome);
-router.get('/events', pageController.getEvents);
+// Home page
+router.get('/', (req, res) => {
+  res.render('pages/index', {
+    title: 'helloRun - Virtual Running Events',
+    user: req.session.user || null
+  });
+});
+
+// About page
+router.get('/about', (req, res) => {
+  res.render('pages/about', {
+    title: 'About Us - helloRun',
+    user: req.session.user || null
+  });
+});
+
+// Blog page
+router.get('/blog', (req, res) => {
+  res.render('pages/blog', {
+    title: 'Blog - helloRun',
+    user: req.session.user || null
+  });
+});
+
+// Events page
+router.get('/events', (req, res) => {
+  res.render('pages/events', {
+    title: 'Browse Events - helloRun',
+    user: req.session.user || null
+  });
+});
+
+// How It Works page
+router.get('/how-it-works', (req, res) => {
+  res.render('pages/how-it-works', {
+    title: 'How It Works - helloRun',
+    user: req.session.user || null
+  });
+});
+
+// Contact page
+router.get('/contact', (req, res) => {
+  res.render('pages/contact', {
+    title: 'Contact Us - helloRun',
+    user: req.session.user || null
+  });
+});
 
 module.exports = router;
